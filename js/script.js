1479a9d02483944a4b8467310ad25c2e176b494f
Silas Matos <silas.progamacao@gmail.com>
2023-09-28 23:13:15 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-function toggleSidebar() {
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.classList.toggle('collapsed')
-}
+$(document).ready(function () {
+  $('#data-table').DataTable({
+    language: {
+      url: 'https://cdn.datatables.net/plug-ins/1.11.5/i18n/pt-BR.json'
+    },
+    pageLength: 10,
+    lengthMenu: [10, 25, 50, 100]
+  })
+})
